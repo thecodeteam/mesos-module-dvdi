8e53945a957997d71229974cf896ba0b6b6c3b13
cantbewong <steven.wong@emc.com>
2016-08-19 15:32:46 -0700

    call sed from script instead embedding sed call in travis.yml

--- set-travis-env.sh
+++ set-travis-env.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
 set -e
 
+# $VER and $TRAVIS_BRANCH are assumed to be set (by travis) when this script is called
+# this is expected to be invoked like this: eval $(./set-travis-env.sh)
+
 GIT_DESC=$(git describe --tags)
 
 REL_MAJOR=$( echo "$GIT_DESC" | sed -rn 's/[^[:digit:]]*([0-9]*).*$/\1/p' )
@@ -14,6 +17,15 @@ V_SHA_LONG=$(git show HEAD -s --format=%H)
 V_EPOCH=$(date +%s)
 V_RELEASE_DATE=$(date +"%Y-%m-%d")
 
+V_SEMVER=${REL_MAJOR}.${REL_MINOR}.${REL_PATCH}
+SEMVER=${V_SEMVER}+${REL_BUILD}
+SO_FILENAME="libmesos_dvdi_isolator-${SEMVER}+mesos-${VER}.so"
+
+sed -e 's/${SEMVER}/'"$V_SEMVER/g" \
+    -e 's|${DSCRIP}|'"$V_SEMVER.Branch.$TRAVIS_BRANCH.Sha.$V_SHA_LONG|g" \
+    -e 's/${RELDTE}/'"$V_RELEASE_DATE/g" \
+    bintray-unstable.json > bintray-unstable-filtered.json
+
 echo "export REL_MAJOR=$REL_MAJOR"
 echo "export REL_MINOR=$REL_MINOR"
 echo "export REL_PATCH=$REL_PATCH"
@@ -22,4 +34,6 @@ echo "export V_BRANCH=$V_BRANCH"
 echo "export V_SHA_LONG=$V_SHA_LONG"
 echo "export V_EPOCH=$V_EPOCH"
 echo "export V_RELEASE_DATE=$V_RELEASE_DATE"
-echo "export V_SEMVER=${REL_MAJOR}.${REL_MINOR}.${REL_PATCH}"
+echo "export V_SEMVER=$V_SEMVER"
+echo "export SEMVER=$SEMVER"
+echo "export SO_FILENAME=$SO_FILENAME"
